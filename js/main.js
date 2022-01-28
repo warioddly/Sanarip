bfbaca50ec104d5c1d839644159b422bc9381ac6
imo <warioddly@gmail.com>
2022-01-28 17:00:03 +0600

    исправил ошибку в секции home

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function(){
-  var header = document.querySelector("header");
+  var header = document.querySelector("nav");
   header.classList.toggle("sticky", window.scrollY > 0);
 })

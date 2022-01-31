604fc426b5b13a136389614d12db5e52b7bae70d
imo <warioddly@gmail.com>
2022-01-31 16:45:48 +0600

    центрирование блоков

--- js/main.js
+++ js/main.js
@@ -3,11 +3,10 @@ window.addEventListener("scroll", function(){
   header.classList.toggle("sticky", window.scrollY > 0);
 })
 
-
 $(window).scroll(function(){
   var $sections = $('section');
   $sections.each(function(i,el){
-  var top  = $(el).offset().top-100;
+  var top  = $(el).offset().top - 200;
   var bottom = top +$(el).height();
   var scroll = $(window).scrollTop();
   var id = $(el).attr('id');
@@ -17,20 +16,3 @@ $(window).scroll(function(){
   }
    })
 });
-
-
-
-mybutton = document.getElementById("myBtn");
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
